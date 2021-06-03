5df6d576473b97cd511521e5ae18dcd35907f314
alamin <alamin@gmail.com>
2021-06-03 09:40:58 +0600

    add singlechildScrollview

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,58 +5,60 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login.png",
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 16,
-              horizontal: 32,
+            SizedBox(
+              height: 20,
             ),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter UserName",
-                    labelText: "UserName",
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 32,
+              ),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "UserName",
+                    ),
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter UserName",
-                    labelText: "UserName",
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "UserName",
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print("Hi alamin");
-                  },
-                  child: Text("Login"),
-                )
-              ],
-            ),
-          )
-        ],
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Hi alamin");
+                    },
+                    child: Text("Login"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
