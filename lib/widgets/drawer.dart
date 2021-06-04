e8a8b9b226929e1a82cadd3dbb3dfd3926a89e11
alamin <alamin@gmail.com>
2021-06-04 10:37:51 +0600

    add listtitle inside drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -7,20 +8,57 @@ class MyDrawer extends StatelessWidget {
         "https://yodafiles.s3-ap-southeast-1.amazonaws.com/uploads/profile_images/thumbs/20190324_181434_5c9774f698ea8.jpg";
     return Drawer(
       child: Container(
-        // color: Colors.deepPurple,
+        color: Colors.blue,
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
             DrawerHeader(
-                padding: EdgeInsets.zero,
-                child: UserAccountsDrawerHeader(
-                  margin: EdgeInsets.zero,
-                  accountName: Text('Alamin Sheikh'),
-                  accountEmail: Text('alamin@gmail.com'),
-                  currentAccountPicture: CircleAvatar(
-                    backgroundImage: NetworkImage(imageURL),
-                  ),
-                ))
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text('Alamin Sheikh'),
+                accountEmail: Text('alamin@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageURL),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email me",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
           ],
         ),
       ),
